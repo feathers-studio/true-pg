8a3d18fe50c5bab5e88ec1d1230900b2dd850174
Muthu Kumar <muthukumar@thefeathers.in>
2025-03-25 22:01:43 +0530

    feat: type fixes and generate comments

--- src/index.ts
+++ src/index.ts
@@ -28,12 +28,15 @@ export interface createGenerator {
 
 export const TruePG: createGenerator = (): Generator => {
 	const column = (col: TableColumn) => {
-		let out = col.name;
+		let out = "";
 
+		if (col.comment) out += `/** ${col.comment} */\n\t`;
+
+		out += col.name;
 		if (col.isNullable) out += "?";
 		// TODO: update imports for non-primitive types
-		out += `: ${col.type.fullName}`;
-		if (col.dimensions) out += "[]".repeat(col.dimensions);
+		out += `: ${col.type.canonical_name}`;
+		if (col.type.dimensions > 0) out += "[]".repeat(col.type.dimensions);
 		if (col.isNullable) out += " | null";
 
 		return out;
@@ -52,7 +55,10 @@ export const TruePG: createGenerator = (): Generator => {
 
 	return {
 		table(table: TableDetails) {
-			let out = `export interface ${table.name} {\n`;
+			let out = "";
+
+			if (table.comment) out += `/** ${table.comment} */\n`;
+			out += `export interface ${table.name} {\n`;
 			for (const col of table.columns) out += `\t${column(col)};\n`;
 			out += "}\n";
 
@@ -60,18 +66,26 @@ export const TruePG: createGenerator = (): Generator => {
 		},
 
 		enum(en: EnumDetails, to: "union" | "enum" = "union") {
+			let out = "";
+
+			if (en.comment) out += `/** ${en.comment} */\n`;
+
 			if (to === "union") {
-				return `export type ${en.name} = ${en.values.map(v => `"${v}"`).join(" | ")};\n`;
+				out += `export type ${en.name} = ${en.values.map(v => `"${v}"`).join(" | ")};\n`;
 			} else {
-				let out = `export enum ${en.name} {\n`;
+				out += `export enum ${en.name} {\n`;
 				for (const v of en.values) out += `\t"${v}" = "${v}",\n`;
 				out += "}\n";
-				return out;
 			}
+
+			return out;
 		},
 
 		composite_type(type: CompositeTypeDetails) {
-			let out = `export interface ${type.name} {\n`;
+			let out = "";
+
+			if (type.comment) out += `/** ${type.comment} */\n`;
+			out += `export interface ${type.name} {\n`;
 
 			const props = type.canonical.attributes.map(c => composite_attribute(c)).map(t => `\t${t};`);
 			out += props.join("\n");
@@ -165,35 +179,44 @@ export async function generate(opts: TruePGOpts) {
 
 	const schemas = await extractor.extractSchemas();
 
+	console.log("Clearing outDir %s", outDir);
 	await rm(outDir, { recursive: true, force: true });
 	await mkdir(outDir, { recursive: true });
 
 	const generator = TruePG();
 
+	console.log("Generating schemas at %s", outDir);
+
 	for (const schema of Object.values(schemas)) {
+		console.log("Generating schema %s", schema.name);
+
 		const schemaDir = `${outDir}/${schema.name}`;
 
 		await mkdir(`${schemaDir}/tables`, { recursive: true });
 
 		for (const table of schema.tables) {
+			console.log("Creating table %s at %s", table.name, `${schemaDir}/tables/${table.name}.ts`);
 			await writeFile(`${schemaDir}/tables/${table.name}.ts`, generator.table(table));
 		}
 
 		await mkdir(`${schemaDir}/enums`, { recursive: true });
 
 		for (const en of schema.enums) {
+			console.log("Creating enum %s at %s", en.name, `${schemaDir}/enums/${en.name}.ts`);
 			await writeFile(`${schemaDir}/enums/${en.name}.ts`, generator.enum(en, opts.enumTo));
 		}
 
 		await mkdir(`${schemaDir}/composite_types`, { recursive: true });
 
 		for (const type of schema.compositeTypes) {
+			console.log("Creating composite type %s at %s", type.name, `${schemaDir}/composite_types/${type.name}.ts`);
 			await writeFile(`${schemaDir}/composite_types/${type.name}.ts`, generator.composite_type(type));
 		}
 
 		await mkdir(`${schemaDir}/functions`, { recursive: true });
 
 		for (const func of schema.functions) {
+			console.log("Creating function %s at %s", func.name, `${schemaDir}/functions/${func.name}.ts`);
 			await writeFile(`${schemaDir}/functions/${func.name}.ts`, generator.function(func));
 		}
 	}
